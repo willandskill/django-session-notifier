bb4908fb9c3da5d86f38d3d132c6d9bd3f0c40f1
Mohammed Hammoud <mohammed@iktw.se>
2016-01-26 10:11:07 +0100

    Make repo pip installable

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-session-notifier',
+    version='0.0.1',
+    author=u'Will & Skill',
+    author_email='info@willandskill.se',
+    packages=find_packages(),
+    url='https://github.com/willandskill/django-session-notifier',
+    license='MIT licence, see LICENCE.txt',
+    description='Displays notification box in all views if user is session authenticated',
+    long_description=open('README.txt').read(),
+    zip_safe=False,
+    include_package_data=True
+)
